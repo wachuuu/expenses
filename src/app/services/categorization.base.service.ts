da06d045d7fba797ebde91a7a6e3b6faaba7ce7a
Gabriel Wachowski <wachowski.gabriel@gmail.com>
2025-02-18 16:04:39 +0100

    make base service accept a single predicate

--- src/app/services/categorization.base.service.ts
+++ src/app/services/categorization.base.service.ts
@@ -7,27 +7,31 @@ import { Transaction } from '../models/transaction.model';
 })
 export abstract class CategorizationBaseService<T> {
 
-  // Abstract property to be implemented by child classes
-  protected abstract mappings: Record<keyof T, (transaction: Transaction) => boolean>;
+  protected abstract mappings: 
+    | Record<keyof T, (transaction: Transaction) => boolean> 
+    | ((transaction: Transaction) => boolean);
 
-  // Generic method to categorize transactions
   public categorize(transactions: Transaction[], initialData: T): CategorisationResult<T> {
-    const categorizedData: T = { ...initialData };
+    const categorizedData: T = structuredClone(initialData);
     const remainingTransactions = transactions.reduce<Transaction[]>((acc, transaction) => {
       let categorized = false;
 
-      for (const [category, isCategory] of Object.entries<(transaction: Transaction) => boolean>(this.mappings)) {
-        if (isCategory(transaction)) {
-          (categorizedData[category as keyof T] as Transaction[]).push(transaction);
+      if (this.mappings instanceof Function) {
+        if (this.mappings(transaction)) {
+          (categorizedData as Transaction[]).push(transaction);
           categorized = true;
-          break;
+        }
+      } else {
+        for (const [category, isCategory] of Object.entries<(transaction: Transaction) => boolean>(this.mappings)) {
+          if (isCategory(transaction)) {
+            ((categorizedData as T)[category as keyof T] as Transaction[]).push(transaction);
+            categorized = true;
+            break;
+          }
         }
       }
 
-      if (!categorized) {
-        acc.push(transaction);
-      }
-
+      if (!categorized) acc.push(transaction);
       return acc;
     }, []);
 
